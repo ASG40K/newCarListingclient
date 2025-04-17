60f0f28fc1ca62807259c3be9882aa75cd7b4e67
ASG40K <anthonygalindo185@gmail.com>
2025-04-17 12:41:10 -0700

    new components

---
+++ src/app/components/add-listing/add-listing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-listing',
+  imports: [],
+  templateUrl: './add-listing.component.html',
+  styleUrl: './add-listing.component.scss'
+})
+export class AddListingComponent {
+
+}
---
+++ src/app/components/car-listings/car-listings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-listings',
+  imports: [],
+  templateUrl: './car-listings.component.html',
+  styleUrl: './car-listings.component.scss'
+})
+export class CarListingsComponent {
+
+}
